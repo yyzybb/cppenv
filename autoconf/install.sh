726ee02fdffb22dc04b2236b6f5ad4be3812d3e0
yuyangzi <yuyangzi@meizu.com>
2015-11-23 15:37:52 +0800

    clang的编译安装单独抽离一个脚本做, 并且放到最后做, 以防耗时太久sudo权限失效

--- autoconf/install.sh
+++ autoconf/install.sh
@@ -20,11 +20,12 @@
 #
 
 # install dos2unix, git, vim, g++, ctags, cmake, python-dev
+
+workdir=`pwd`
 sudo ./change_source_list.sh
 
 VUNDLE_GIT=https://code.csdn.net/u014579048/vundle-vim.git
 YCM_GIT=https://code.csdn.net/u014579048/youcompleteme.git
-LLVM_CLANG_GIT=https://code.csdn.net/u014579048/llvm-clang.git
 CMAKE_GIT=https://code.csdn.net/u014579048/cmake.git
 
 #VUNDLE_GIT=https://github.com/gmarik/Vundle.vim.git
@@ -69,15 +70,14 @@ vim_path=$HOME/.vim
 vundle_path=${vim_path}/vimfiles/bundle/Vundle.vim
 #echo $vim_path
 #echo ${vundle_path}
-while true
-do
-    if test -d ${vundle_path}; then
-        cd ${vundle_path}
-        git pull && break
-    else
-        git clone ${VUNDLE_GIT} ${vundle_path} && break
-    fi
-done
+
+if test -d ${vundle_path}; then
+    cd ${vundle_path}
+    git pull
+    cd -
+else
+    git clone ${VUNDLE_GIT} ${vundle_path}
+fi
 
 # install vim-plugins
 vim +BundleInstall -c quitall
@@ -99,39 +99,7 @@ build_llvm_clang()
     echo 'Not found python-clang in sys source, will install there from llvm-clang source code.'
 
     llvm_clang_dir=$HOME/llvm-clang_cppenv
-    if test -d $llvm_clang_dir; then
-        cd $llvm_clang_dir
-        git pull
-    else
-        git clone $LLVM_CLANG_GIT $llvm_clang_dir
-        cd $llvm_clang_dir
-    fi
-
-    xz -dk llvm.tar.xz || echo ''
-    xz -dk clang.tar.xz || echo ''
-    tar xf clang.tar
-    tar xf llvm.tar
-
-    cd llvm-3.7.0.src
-    mkdir -p build
-    cd build
-    cmake ..
-    make
-    sudo make install
-    cd ../..
-
-    cd cfe-3.7.0.src
-    mkdir -p build
-    cd build
-    cmake ..
-    make
-    sudo make install
-    cd ..
-
-    pyclang_dst=/usr/lib/python2.7/dist-packages
-    test -d $pyclang_dst || pyclang_dst=/usr/lib/python2.7/site-packages
-    sudo cp bindings/python/clang $pyclang_dst -r
-    sudo echo '/usr/local/lib' > /etc/ld.so.conf.d/cppenv.conf
+    sudo ${workdir}/install_clang.sh
 }
 
 install_cmake_from_source_list()
---
+++ autoconf/install_clang.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+
+llvm_clang_dir=$1
+LLVM_CLANG_GIT=https://code.csdn.net/u014579048/llvm-clang.git
+if test -d $llvm_clang_dir; then
+    cd $llvm_clang_dir
+    git pull
+else
+    git clone $LLVM_CLANG_GIT $llvm_clang_dir
+    cd $llvm_clang_dir
+fi
+
+xz -dk llvm.tar.xz || echo ''
+xz -dk clang.tar.xz || echo ''
+tar xf clang.tar
+tar xf llvm.tar
+
+cd llvm-3.7.0.src
+mkdir -p build
+cd build
+cmake ..
+make
+sudo make install
+cd ../..
+
+cd cfe-3.7.0.src
+mkdir -p build
+cd build
+cmake ..
+make
+sudo make install
+cd ..
+
+pyclang_dst=/usr/lib/python2.7/dist-packages
+test -d $pyclang_dst || pyclang_dst=/usr/lib/python2.7/site-packages
+cp bindings/python/clang $pyclang_dst -r
+echo '/usr/local/lib' > /etc/ld.so.conf.d/cppenv.conf
