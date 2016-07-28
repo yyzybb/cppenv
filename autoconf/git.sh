ccc11ae0499d720b9f967b95dbaa5dcd21e662ea
yuyangzi <yuyangzi@meizu.com>
2016-07-28 00:36:45 -0700

    alias lgf = git log --name-status

--- autoconf/git.sh
+++ autoconf/git.sh
@@ -4,8 +4,11 @@ user_name="test"
 
 sudo cp ./git_cmd/* /usr/local/bin
 
-git config --global user.email $user_name@gmail.com
-git config --global user.name $user_name
+if [ `git config --global --list | grep user | wc -l` -eq 0 ]
+then
+    git config --global user.email $user_name@gmail.com
+    git config --global user.name $user_name
+fi
 
 git config --global diff.tool vimdiff
 git config --global diff.prompt false
@@ -18,6 +21,7 @@ git config --global alias.ss status
 git config --global alias.co checkout
 git config --global alias.cmt commit
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.lgf "log --name-status"
 
 git config --global receive.denyCurrentBranch true
 git config --global core.editor vim
