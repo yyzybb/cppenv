c02e495940b4c1183b69cf886e4f1c38ae880f43
yuyangzi <yuyangzi@meizu.com>
2017-12-18 00:06:30 -0800

    add goinstall

--- autoconf/goenv.sh
+++ autoconf/goenv.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+go get -v github.com/yyzybb537/goinstall
 go get -v github.com/kardianos/govendor
 go get -v github.com/golang/tools
 go get -v github.com/nsf/gocode
@@ -8,4 +9,3 @@ go get -v github.com/bradfitz/goimports
 go get -v github.com/bytbox/golint
 go get -v github.com/jstemmer/gotags
 go get -v github.com/kisielk/errcheck
-
