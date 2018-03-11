f14475545e55aabc55ae5d55937440f42643b2bd
test <test@gmail.com>
2018-03-11 15:31:02 +0800

    修复编译vim的问题

--- autoconf/go.sh
+++ autoconf/go.sh
@@ -18,36 +18,28 @@ then
     source /etc/profile
 fi
 
-cd ~
-if test ! -d go
-then 
-    git clone https://git.oschina.net/yyzybb537/go.git
-else
-    cd go
-    git pull
-    cd ~
-fi
-
-if [ `which go | wc -l` -eq 0 ]
-then
-    cp go go1.4 -r
-    cd go1.4/src
-    git checkout release-branch.go1.4
-    ./all.bash
-    cd -
-    cd go/src
-    git checkout release-branch.go1.7
-    ./all.bash
-    cd -
-fi
-
-#DIR=/usr/lib/go/src/golang.org/x
-#mkdir -p $DIR
-#cd $DIR
-#test ! -d tools && git clone https://github.com/golang/tools.git
+#cd ~
+#if test ! -d go
+#then 
+#    git clone https://git.oschina.net/yyzybb537/go.git
+#else
+#    cd go
+#    git pull
+#    cd ~
+#fi
 #
-#cd $DIR/tools/cmd/gorename && go build && go install
-#cd $DIR/tools/cmd/oracle && go build && go install
+#if [ `which go | wc -l` -eq 0 ]
+#then
+#    cp go go1.4 -r
+#    cd go1.4/src
+#    git checkout release-branch.go1.4
+#    ./all.bash
+#    cd -
+#    cd go/src
+#    git checkout release-branch.go1.7
+#    ./all.bash
+#    cd -
+#fi
 
 cd $workdir
 ./goenv.sh
